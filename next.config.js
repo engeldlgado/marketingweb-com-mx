1bc53f2d504bb466dc9ea8e7e9757630792d492a
Engelbert Vizcaya <engeldlgado@gmail.com>
2023-04-04 12:28:50 -0400

    UPDATE: Redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,80 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects () {
+    return [
+      {
+        source: '/seo-wordpress.html',
+        destination: '/posicionamiento-web',
+        permanent: true
+      },
+      {
+        source: '/posicionamiento-web.html',
+        destination: '/posicionamiento-web',
+        permanent: true
+      },
+      {
+        source: '/seo-drupal.html',
+        destination: '/posicionamiento-web',
+        permanent: true
+      },
+      {
+        source: '/seo-joomla.html',
+        destination: '/posicionamiento-web',
+        permanent: true
+      },
+      {
+        source: '/diseno-web-wordpress.html',
+        destination: '/diseno-web',
+        permanent: true
+      },
+      {
+        source: '/diseno-web.html',
+        destination: '/diseno-web',
+        permanent: true
+      },
+      {
+        source: '/migraciones-de-drupal-a-wordpress.html',
+        destination: '/diseno-web',
+        permanent: true
+      },
+      {
+        source: '/migraciones-de-joomla-a-wordpress.html',
+        destination: '/diseno-web',
+        permanent: true
+      },
+      {
+        source: '/sistemas-de-reservaciones.html',
+        destination: '/diseno-web',
+        permanent: true
+      },
+      {
+        source: '/redes-sociales.html',
+        destination: '/redes-sociales',
+        permanent: true
+      },
+      {
+        source: '/marketing-digital.html',
+        destination: '/marketing-digital',
+        permanent: true
+      },
+      {
+        source: '/google-shoping.html',
+        destination: '/marketing-digital',
+        permanent: true
+      },
+      {
+        source: '/google-my-business.html',
+        destination: '/marketing-digital',
+        permanent: true
+      },
+      {
+        source: '/pagina-negocios-facebook.html',
+        destination: '/marketing-digital',
+        permanent: true
+      }
+    ]
+  },
   images: {
     domains: ['mlrsjyafnwfj.i.optimole.com', 'secure.gravatar.com']
   },
