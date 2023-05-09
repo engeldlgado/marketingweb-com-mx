e3388ca12c965e0f8d638f16befbfc0775889082
Engelbert Vizcaya <engeldlgado@gmail.com>
2023-05-09 15:59:41 -0400

    UPDATE: FB Pixel

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -1,6 +1,7 @@
 import { useStore } from '@/Context/Store'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
+import Script from 'next/script'
 import { useEffect, useRef, useState } from 'react'
 import Menu from '../navigation/Menu'
 import { Footer } from '../sections/Footer'
@@ -78,6 +79,30 @@ const Layout = ({ children, title, description, keywords, ogType, ogUrl, ogImage
           ))
         )}
       </Head>
+      {/* FB Pixel Script */}
+      <Script
+        id='fb-pixel'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2635680783136016');
+          fbq('track', 'PageView');
+          `
+        }}
+      />
+      <noscript><img
+        height='1' width='1' style='display:none'
+        src='https://www.facebook.com/tr?id=2635680783136016&ev=PageView&noscript=1'
+                />
+      </noscript>
       <header>
         <Menu
           theme={theme}
